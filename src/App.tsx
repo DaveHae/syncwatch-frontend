301435d25d062012a53b49b905efd8e45df0c5a2
DaveHae <davehae99@gmail.com>
2021-03-03 21:52:29 +0100

    Add: websocket example in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import Search from './components/search/Search';
 import Player from './components/player/Player';
 import Queue from './components/queue/Queue';
 import Video from './model/Video';
+import { io } from 'socket.io-client';
+const ENDPOINT = 'http://127.0.0.1:7000';
 
-class App extends React.Component {
-  videos: Video[] = [
+const App = () => {
+  const [response, setResponse] = useState('');
+
+  useEffect(() => {
+    const socket = io(ENDPOINT);
+    socket.on('FromAPI', (data: any) => {
+      setResponse(data);
+    });
+  }, []);
+
+  const videos: Video[] = [
     {
       title: 'Video 1',
       img: 'https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg',
@@ -29,23 +40,24 @@ class App extends React.Component {
     },
   ];
 
-  render() {
-    return (
-      <div className="App">
-        <header className="app-header">
-          <div className="logo-name">
-            <img className="logo" alt="logo" src="/logo.png" />
-            <h1>SyncWatch</h1>
-          </div>
-          <Search />
-        </header>
-        <div className="main">
-          <Player url={'https://www.youtube.com/embed/dQw4w9WgXcQ'} />
-          <Queue videos={this.videos} />
+  return (
+    <div className="App">
+      <header className="app-header">
+        <div className="logo-name">
+          <img className="logo" alt="logo" src="/logo.png" />
+          <h1>SyncWatch</h1>
         </div>
+        <Search />
+      </header>
+      <div className="main">
+        <Player url={'https://www.youtube.com/embed/dQw4w9WgXcQ'} />
+        <Queue videos={videos} />
       </div>
-    );
-  }
-}
+      <p>
+        It's <time dateTime={response}>{response}</time>
+      </p>
+    </div>
+  );
+};
 
 export default App;
--- src/components/queue/Queue.tsx
+++ src/components/queue/Queue.tsx
@@ -6,26 +6,25 @@ interface IProps {
   videos: Video[];
 }
 
-function Queue(props: IProps) {
-    return (
-      <div className="queue">
-        <h3>Queue:</h3>
-        {props.videos.map((video) => {
-          return (
-            <div className="queue-entry" key={video.title}>
-              <div className="queue-image">
-                <img src={video.img} alt="YouTube Thumbnail" />
-              </div>
-              <div className="queue-title">{video.title}</div>
-              <div className="queue-requestedBy">
-                <img src={video.requestedBy.img} alt="Requested By" />
-              </div>
+const Queue = (props: IProps) => {
+  return (
+    <div className="queue">
+      <h3>Queue:</h3>
+      {props.videos.map((video) => {
+        return (
+          <div className="queue-entry" key={video.title}>
+            <div className="queue-image">
+              <img src={video.img} alt="YouTube Thumbnail" />
             </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+            <div className="queue-title">{video.title}</div>
+            <div className="queue-requestedBy">
+              <img src={video.requestedBy.img} alt="Requested By" />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Queue;
